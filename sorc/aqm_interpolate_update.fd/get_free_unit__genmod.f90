22f0a39a82be2750493643dc6974469918264446
jianping.huang@noaa.gov <jianping.huang@noaa.gov>
2016-10-13 13:37:42 +0000

    commit after testing on Surge

--- sorc/aqm_interpolate_update.fd/get_free_unit__genmod.f90
+++ sorc/aqm_interpolate_update.fd/get_free_unit__genmod.f90
@@ -1,4 +1,4 @@
-        !COMPILER-GENERATED INTERFACE MODULE: Wed Sep 28 15:58:39 2016
+        !COMPILER-GENERATED INTERFACE MODULE: Thu Oct  6 19:16:00 2016
         ! This source file is for reference only and may not completely
         ! represent the generated interface used by the compiler.
         MODULE GET_FREE_UNIT__genmod
