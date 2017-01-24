72c6f0d594e1f72e749d5529fc21cd6b06b5b3cc
jianping.huang@noaa.gov <jianping.huang@noaa.gov>
2017-01-24 17:12:38 +0000

    adding *mod files t aqm_premaq_v502.fd

--- sorc/aqm_interpolate_update.fd/message__genmod.f90
+++ sorc/aqm_interpolate_update.fd/message__genmod.f90
@@ -1,4 +1,4 @@
-        !COMPILER-GENERATED INTERFACE MODULE: Wed Jan 11 16:58:31 2017
+        !COMPILER-GENERATED INTERFACE MODULE: Tue Jan 24 15:24:50 2017
         ! This source file is for reference only and may not completely
         ! represent the generated interface used by the compiler.
         MODULE MESSAGE__genmod
