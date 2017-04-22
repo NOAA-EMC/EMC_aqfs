cfa9ca9ae76fc23ff1c311e05c12e95f14939edd
jianping.huang@noaa.gov <jianping.huang@noaa.gov>
2017-04-22 04:14:03 +0000

    addign a change for sending aqm.t??z.1hr_ave_pm25_bc.227.grib2 to ~/com

--- sorc/aqm_interpolate_update.fd/message__genmod.f90
+++ sorc/aqm_interpolate_update.fd/message__genmod.f90
@@ -1,4 +1,4 @@
-        !COMPILER-GENERATED INTERFACE MODULE: Sat Apr 15 04:30:08 2017
+        !COMPILER-GENERATED INTERFACE MODULE: Fri Apr 21 20:05:58 2017
         ! This source file is for reference only and may not completely
         ! represent the generated interface used by the compiler.
         MODULE MESSAGE__genmod
