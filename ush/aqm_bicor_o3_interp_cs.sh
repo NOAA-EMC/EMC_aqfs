2f080c31e24f9954acbdb663c57a2efb662fc758
Ho-Chun Huang <Ho-Chun.Huang@noaa.gov>
2021-03-30 16:13:23 +0000

    incremental changes for modifying ecflow script for NCO's reference

--- ush/aqm_bicor_o3_interp_cs.sh
+++ ush/aqm_bicor_o3_interp_cs.sh
@@ -13,36 +13,29 @@ set -xa
 
 export DBNALERT_TYPE=${DBNALERT_TYPE:-GRIB_HIGH}
 
-cd $DATA
+cd ${DATA}
 
 mkdir -p data/coords site-lists
 
-mkdir -p out/ozone/$Yr
+mkdir -p out/ozone/${Yr}
 
 #===================================================`
-#ln -s $PARMaqm/sites.valid.ozone.20200815.06z.list  $DATA/site-lists
-if [ -s $COMINm1/sites.valid.ozone.$PDYm1.12z.list ] ; then
- ln -s $COMINm1/sites.valid.ozone.$PDYm1.12z.list    $DATA/site-lists/sites.valid.ozone.12z.list
-else
- ln -s $PARMaqm/sites.valid.ozone.20210313.12z.list  $DATA/site-lists/sites.valid.ozone.12z.list
-fi
+ln -s $PARMaqm/sites.valid.ozone.20210328.12z.list  ${DATA}/site-lists
 
-ln -s $PARMaqm/aqm.t12z.grdcro2d.ncf    $DATA/data/coords
-ln -s $PARMaqm/aqm_config.interp.ozone.8-vars  $DATA
-ln -s ${COMINbicor}   $DATA/data
+ln -s $PARMaqm/aqm.t12z.grdcro2d.ncf    ${DATA}/data/coords
+ln -s $PARMaqm/aqm_config.interp.ozone.8-vars  ${DATA}
+ln -s ${COMINbicor}   ${DATA}/data
 
 startmsg
-$EXECaqm/aqm_interpolate_update  aqm_config.interp.ozone.8-vars ${cyc}z ${bc_interp_hr} $PDY $PDY  >> $pgmout 2>errfile 
+$EXECaqm/aqm_interpolate_update aqm_config.interp.ozone.8-vars ${cyc}z ${bc_interp_hr} ${PDY} ${PDY}  >> $pgmout 2>errfile 
 export err=$?;err_chk
 
-if [ -e ${COMINbicor}/interpolated/ozone/$Yr ] 
+if [ -e ${COMINbicor}/interpolated/ozone/${Yr} ] 
 then 
-  cp -p $DATA/out/ozone/$Yr/*nc  ${COMINbicor}/interpolated/ozone/$Yr
+  cp -p ${DATA}/out/ozone/${Yr}/*nc  ${COMINbicor}/interpolated/ozone/${Yr}
 else
-  mkdir -p ${COMINbicor}/interpolated/ozone/$Yr
-  cp -p  $DATA/out/ozone/$Yr/*nc  ${COMINbicor}/interpolated/ozone/$Yr
+  mkdir -p ${COMINbicor}/interpolated/ozone/${Yr}
+  cp -p  ${DATA}/out/ozone/${Yr}/*nc  ${COMINbicor}/interpolated/ozone/${Yr}
 fi
 
-#cp -p $DATA/sites/sites.valid.ozone.$PDY.${cyc}z.list  $COMIN
-
-echo "Interploation is done for " $PDY 
+echo "Interploation is done for " ${PDY} 
--- ush/aqm_bicor_pm25_interp_cs.sh
+++ ush/aqm_bicor_pm25_interp_cs.sh
@@ -15,35 +15,29 @@ export DBNALERT_TYPE=${DBNALERT_TYPE:-GRIB_HIGH}
 
 export bc_fcst_hr=72
 
-cd $DATA
+cd ${DATA}
 
 mkdir -p data/coords site-lists
 
-mkdir -p out/pm25/$Yr
+mkdir -p out/pm25/${Yr}
 
 #===================================================`
-#ln -s $PARMaqm/aqm_sites.valid.pm25.20190815.06z.list  $DATA/site-lists
-if [ -s $COMINm1/sites.valid.pm25.${PDYm1}.12z.list ] ; then
- ln -s $COMINm1/sites.valid.pm25.${PDYm1}.12z.list     $DATA/site-lists/sites.valid.pm25.12z.list
-else
- ln -s $PARMaqm/sites.valid.pm25.20210313.12z.list $DATA/site-lists/sites.valid.pm25.12z.list
-fi
+ln -s $PARMaqm/sites.valid.pm25.20210328.12z.list ${DATA}/site-lists
 
-ln -s $PARMaqm/aqm.t12z.grdcro2d.ncf    $DATA/data/coords
-ln -s $PARMaqm/aqm_config.interp.pm2.5.5-vars  $DATA
-ln -s ${COMINbicor}   $DATA/data
+ln -s $PARMaqm/aqm.t12z.grdcro2d.ncf    ${DATA}/data/coords
+ln -s $PARMaqm/aqm_config.interp.pm2.5.5-vars  ${DATA}
+ln -s ${COMINbicor}   ${DATA}/data
 
 startmsg
-#$EXECaqm/aqm_interpolate_update  aqm_config.interp.pm2.5.5-vars ${cyc}z ${bc_interp_hr} $PDY $PDY  >> $pgmout 2>errfile 
-$EXECaqm/aqm_interpolate_update  aqm_config.interp.pm2.5.5-vars ${cyc}z ${bc_interp_hr} $PDY $PDY  > interp_pm25.log
+$EXECaqm/aqm_interpolate_update aqm_config.interp.pm2.5.5-vars ${cyc}z ${bc_interp_hr} ${PDY} ${PDY}  >> $pgmout 2>errfile
 export err=$?;err_chk
 
-if [ -e ${COMINbicor}/interpolated/pm25/$Yr ] 
+if [ -e ${COMINbicor}/interpolated/pm25/${Yr} ] 
 then 
-  cp -p $DATA/out/pm25/$Yr/*nc  ${COMINbicor}/interpolated/pm25/$Yr
+  cp -p ${DATA}/out/pm25/${Yr}/*nc  ${COMINbicor}/interpolated/pm25/${Yr}
 else
-  mkdir -p ${COMINbicor}/interpolated/pm25/$Yr
-  cp -p  $DATA/out/pm25/$Yr/*nc  ${COMINbicor}/interpolated/pm25/$Yr
+  mkdir -p ${COMINbicor}/interpolated/pm25/${Yr}
+  cp -p  ${DATA}/out/pm25/${Yr}/*nc  ${COMINbicor}/interpolated/pm25/${Yr}
 fi
 
-echo "Interploation is done for " $PDY 
+echo "Interploation is done for " ${PDY} 
