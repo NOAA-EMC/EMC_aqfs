008844f921244a5ce4f0caff44cac49b053e65fb
Ho-Chun Huang <Ho-Chun.Huang@noaa.gov>
2021-05-21 23:37:13 +0000

    replace csh script with bash shell

--- ush/aqm_bicor_prep_cs.sh
+++ ush/aqm_bicor_prep_cs.sh
@@ -6,23 +6,9 @@
 echo "It is extracting PM2.5 and met inputs for Bias Correction"
 
 #
-## No longer used, commented out
-#
-## cd $DATA
-## startmsg
-## $USHaqm/aqm_subset.sh -l "1 1" -s "PRES DENS QV QR" $COMIN/aqm.$cycle.metcro3d.ncf Spec_humid.${cycle}.ncf  >> $pgmout 2>errfile
-## export err=$?;err_chk
-
 startmsg
-$USHaqm/aqm_subset.sh -s "PRSFC TEMPG TEMP2 WSPD10 WDIR10 RGRND RN RC HFX LH CFRAC PBL" $COMIN/aqm.$cycle.metcro2d.ncf sfc_met_n_PBL.${cycle}.ncf >> $pgmout 2>errfile
+$USHaqm/aqm_subset.sh "PRSFC TEMPG TEMP2 WSPD10 WDIR10 RGRND RN RC HFX LH CFRAC PBL" $COMIN/aqm.$cycle.metcro2d.ncf sfc_met_n_PBL.${cycle}.ncf >> $pgmout 2>errfile
 export err=$?;err_chk
-
-#
-## No longer used, commented out
-#
-## startmsg
-## $USHaqm/aqm_subset.sh -l "1 1" -s "UWIND VWIND" $COMIN/aqm.$cycle.metdot3d.ncf UV_wind.${cycle}.ncf  >> $pgmout 2>errfile
-## export err=$?;err_chk
 
 startmsg
 $USHaqm/aqm_subc.sh  $PARMaqm/aqm_temp25pm_aero6_gas $COMIN/aqm.$cycle.aconc_sfc.ncf aqm.${cycle}.O3_pm25.ncf >> $pgmout 2>errfile
